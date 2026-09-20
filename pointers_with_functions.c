7cf2f024db706622b59ca330c861095177f71e54
Nesara M S <nesarakushal@gmail.com>
2026-09-20 21:01:33 +0530

    Add files via upload

--- pointers_with_functions.c
+++ pointers_with_functions.c
@@ -1,14 +1,33 @@
 // Pointers with Functions
 #include <stdio.h>
 
-void modifyValue(int *ptr) {
-    *ptr = 10;
+void SwapWithAddress(int *n1, int *n2) 
+{
+    int temp;
+    temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
 }
+void SwapWithoutAddress(int n1, int n2)
+{
+    int temp;
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+int main ()
+{
+    int num1 = 5, num2 = 10;
+    //num1 and num2 are passed by value to the function SwapWithoutAddress
+    SwapWithoutAddress(num1, num2);
+    printf("After Swaping without address\n");
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d\n\n", num2);
 
-int main() {
-    int value = 5;
-    printf("Before modification: %d\n", value);
-    modifyValue(&value);
-    printf("After modification: %d\n", value);
-    return 0;
+    //address of num1 and num2 are passed by reference to the function SwapWithAddress
+    SwapWithAddress(&num1, &num2);
+    printf("After Swaping with address\n");
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d\n", num2);
+
 }
